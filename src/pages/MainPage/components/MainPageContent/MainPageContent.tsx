0d1d2b84e12f1cc973baa8e79b0c2849ea2aa686
Dmytro Hopko <gopkodimon@gmail.com>
2025-04-12 17:13:05 +0000

    feat: created container

--- src/pages/MainPage/components/MainPageContent/MainPageContent.tsx
+++ src/pages/MainPage/components/MainPageContent/MainPageContent.tsx
@@ -2,5 +2,7 @@ import './MainPageContent.scss';
 import { JSX } from 'react';
 
 export const MainPageContent = (): JSX.Element => {
-  return <section className="main_page">Main Page Content Area</section>;
+  return (
+    <section className="main_page container">Main Page Content Area</section>
+  );
 };
